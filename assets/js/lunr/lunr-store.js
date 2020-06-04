8daea75eb4e9bd61b1751b21cee22e9ab6b60e90
josecastillolema <josecastillolema@gmail.com>
2020-06-04 12:12:13 -0300

    new post

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,181 @@
+var store = [{
+        "title": "IEEE GLOBECOM 2013",
+        "excerpt":"Globecom 2013 Workshop - Management of Emerging Networks and Services Program Program Hybrid Framework for Scalable Resource Control in Multi-ingress Networks Sandino Jardim (Federal University of Goias, Brazil) Augusto Jose Venancio Neto, Ph. D. (Universidade Federal do Rio Grande do Norte, Brazil) Jose Castillo Lema (Universidade da Coruña, Spain) Evariste...","categories": [],
+        "tags": ["events","networks"],
+        "url": "http://localhost:4000/globecom/",
+        "teaser": null
+      },{
+        "title": "ISCC 2014",
+        "excerpt":"THE NINETEENTH IEEE SYMPOSIUM ON COMPUTERS AND COMMUNICATIONS Program Entity Title Architecture Extensions Towards Advanced Quality-oriented Mobility Control Capabilities Felipe Sampaio Dantas da Silva (Federal Institute of Education, Science and Technology of Rio Grande do Norte, Brazil) José Castillo Lema (Universidade Federal do Rio Grande do Norte, Spain) Augusto Jose...","categories": [],
+        "tags": ["events","networks"],
+        "url": "http://localhost:4000/iscc/",
+        "teaser": null
+      },{
+        "title": "IEEE GLOBECOM 2014",
+        "excerpt":"Global Communications Conference 2014 Additions to the ETArch control plane to support multimedia QoS-guaranteed content transport over OpenFlow-enabled SDN future internet systems José Castillo - Federal University of Rio Grande do Norte (UFRN), Brazil Augusto Neto - Federal University of Rio Grande do Norte (UFRN), Brazil Flavio Silva - Federal...","categories": [],
+        "tags": ["events","networks"],
+        "url": "http://localhost:4000/globecom/",
+        "teaser": null
+      },{
+        "title": "FIAP MBA em Full Stack Developer - Microservices, Cloud e IoT (SCJ)",
+        "excerpt":"   Arquitetura e Desenvolvimento Java com IoT   Arquitetura e Desenvolvimento Java de Alta Disponibilidade para Cloud Computing   Arquitetura e Infraestrutura de Cloud Computing e Internet das Coisas   Cloud Development   Desenvolvimento de aplicações usando Cloud Computing     ","categories": [],
+        "tags": ["classes"],
+        "url": "http://localhost:4000/fiap-mba-scj/",
+        "teaser": null
+      },{
+        "title": "Computer Networks - Volume 107",
+        "excerpt":"   Volume 107, Part 2, Pages 270-291   An innovative software-defined WiNeMO architecture for advanced QoS-guaranteed mobile service transport      Felipe S. Dantas Silva   Augusto Venâncio Netob   Douglas Maciel   José Castillo-Lema   Flávio Silva   Pedro Frosi   EduardoCerqueira.  ","categories": [],
+        "tags": ["networks"],
+        "url": "http://localhost:4000/computer-networks/",
+        "teaser": null
+      },{
+        "title": "FIAP MBA em Mobile Development (MOB)",
+        "excerpt":"   DevOps &amp; Cloud Oriented Architecture   Segurança para Aplicações Móveis para Internet das Coisas e Cloud Computing     ","categories": [],
+        "tags": ["classes"],
+        "url": "http://localhost:4000/fiap-mba-mob/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 1st edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-01/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 2nd edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-02/",
+        "teaser": null
+      },{
+        "title": "PUC-SP Cursos de extensão - Internet das Coisas",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes"],
+        "url": "http://localhost:4000/puc/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 3rd edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-03/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 4th edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-04/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 5th/6th edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-05/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 7th edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-07/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 8th edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-08/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 9th edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-09/",
+        "teaser": null
+      },{
+        "title": "Futurecom 2017",
+        "excerpt":"   ","categories": [],
+        "tags": ["events"],
+        "url": "http://localhost:4000/futurecom2017/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 10th edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-10/",
+        "teaser": null
+      },{
+        "title": "[PT] Terraform – IaC – Terraformando no OpenStack",
+        "excerpt":"Olá pessoal, vamos começar uma uma série de artigos sobre OpenStack dando continuidade aos artigos sobre Terraform (parte 1 e parte 2) do Rodrigo Floriano, pois é uma ferramenta que vários assíduos do blog já conhecem e usam a diário! Hoje vamos mostrar um exemplo prático de uso da ferramenta...","categories": [],
+        "tags": ["openstack","iac","terraform"],
+        "url": "http://localhost:4000/terraformando-openstack/",
+        "teaser": null
+      },{
+        "title": "[PT] Heat - Introdução",
+        "excerpt":"Olá pessoal, vamos começar uma série de artigos sobre Heat, o “terraform” nativo do OpenStack, ou para quem vem do mundo da AWS, o CloudFormation do OpenStack (inclusive parcialmente compatível). Neste primeiro artigo introdutório vamos aprender a criar as nossas próprias pilhas (ou stacks) e recriar a instância do servidor...","categories": [],
+        "tags": ["iac","openstack"],
+        "url": "http://localhost:4000/heat-introducao/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 11th edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-11/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 12th edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-12/",
+        "teaser": null
+      },{
+        "title": "HCNA-HNTD Training - 13th edition",
+        "excerpt":"  ","categories": [],
+        "tags": ["classes","networks"],
+        "url": "http://localhost:4000/hntd-13/",
+        "teaser": null
+      },{
+        "title": "Webinar Mandic Cloud - IPv6 no OpenStack",
+        "excerpt":"   ","categories": [],
+        "tags": ["events","webinar"],
+        "url": "http://localhost:4000/mandic-webinar/",
+        "teaser": null
+      },{
+        "title": "AWS Educate Cloud Ambassador",
+        "excerpt":"AWS Educate     ","categories": [],
+        "tags": ["aws"],
+        "url": "http://localhost:4000/abs-ambassador/",
+        "teaser": null
+      },{
+        "title": "SBRC/WPEIF 2019",
+        "excerpt":"XXXVII Simpósio Brasileiro de Redes de Computadores e Sistemas Distribuídos / Workshop de Pesquisa Experimental da Internet do Futuro Proceedings Network Function Virtualization in Content-Centric Networks José Castillo Lema, Universidade de São Paulo, Brazil Augusto J. Venancio Neto, Federal University of Rio Grande do Norte, Brazil Flavio de Oliveira Silva,...","categories": [],
+        "tags": ["events","networks"],
+        "url": "http://localhost:4000/sbrc19/",
+        "teaser": null
+      },{
+        "title": "11a Semana de TI da Sumaré",
+        "excerpt":"Agenda        ","categories": [],
+        "tags": ["events"],
+        "url": "http://localhost:4000/sumare/",
+        "teaser": null
+      },{
+        "title": "IEEE NetSoft/S4SI 2019",
+        "excerpt":"IEEE Conference on Network Softwarization / 2nd Workshop on Advances in Slicing for Softwarized Infrastructures Agenda Mininet-NFV: Evolving Mininet with OASIS TOSCA NVF profiles Towards Reproducible NFV Prototyping José Castillo Lema, Universidade de São Paulo, Brazil Augusto J. Venancio Neto, Federal University of Rio Grande do Norte, Brazil Flavio de...","categories": [],
+        "tags": ["events","networks"],
+        "url": "http://localhost:4000/netsof/",
+        "teaser": null
+      },{
+        "title": "A generic NFV MANO for Content-Centric Networks",
+        "excerpt":"Biblioteca Digital de Teses e Dissertações da USP ISO CASTILLO LEMA, José. A generic network function virtualization manager and orchestrator for content-centric networks [doi:10.11606/T.3.2019.tde-23102019-120559]. São Paulo : Escola Politécnica, Universidade de São Paulo, 2019. Tese de Doutorado em Sistemas Eletrônicos. [acesso 2020-06-01]. ABNT CASTILLO LEMA, José. A generic network function...","categories": [],
+        "tags": ["networks"],
+        "url": "http://localhost:4000/thesis/",
+        "teaser": null
+      },{
+        "title": "OpenShift 4.3 on RHOSP 13: Installation and Integrations",
+        "excerpt":"I would like to share my experience installing and integrating OpenShift with various OpenStack services, namely Cinder, Swift and Keystone (Neutron and Octavia TBD). Prerequisites This was not a disconnected environment. Note that virtual machines have to be born with proper DNS configuration for OpenShift to download proper CoreOS images...","categories": [],
+        "tags": ["openstack","openshift"],
+        "url": "http://localhost:4000/osp43-on-rhosp13/",
+        "teaser": null
+      },{
+        "title": "aodh2sensu - importing alarms from Aodh into Sensu Core",
+        "excerpt":"aodh2sensu Imports OpenStack Aodh alarms into Sensu Core Server. Install $ pip3 install -r ./requirements.txt Use Usage: aodh2sensu.py [options] Imports OpenStack Aodh alarms into Sensu Core Server. Options: -h, --help show this help message and exit --sensu-url URL if not specified, defaults to localhost:4567 Run the aodh2sensu proxy. sensu_url must...","categories": [],
+        "tags": ["openstack","monitoring"],
+        "url": "http://localhost:4000/aodh2sensu/",
+        "teaser": null
+      }]
