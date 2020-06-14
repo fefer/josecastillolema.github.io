8e85cbc3a2374e154d09cec35878363ad8ba47c5
josecastillolema <josecastillolema@gmail.com>
2020-06-14 10:45:41 -0300

    added _site folder

---
+++ build.sh
@@ -0,0 +1,11 @@
+set -v
+bundle exec jekyll build
+git add *
+git commit -m "auto build"
+git push
+cp -r _site/* ../josecastillolema.github.io/
+cd ../josecastillolema.github.io/
+git add *
+git commit -m "new post"
+git push
+set +v
